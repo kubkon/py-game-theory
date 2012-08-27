110d31e8a55f374643e822d83eab9b353eb1b42a
Jakub Konka <kubkon@gmail.com>
2012-08-27 14:18:02 +0200

    Try to programmatically obtain list of all possible probability vectors for each player (messy!)

--- strategic_form.py
+++ strategic_form.py
@@ -1,6 +1,10 @@
 from numpy import array, linspace
 from itertools import chain
 
+def operator(*args):
+  vector = [el for el in args]
+  return vector + [1 - sum(vector)]
+
 def transpose(payoff_matrix):
   return array(payoff_matrix).transpose().tolist()
 
@@ -23,16 +27,27 @@ def solve_psne_2(payoff_matrix_p1, payoff_matrix_p2):
 def solve_msne_2(payoff_matrix_p1, payoff_matrix_p2, granularity=5):
   # Construct list of probability values
   probability_range = linspace(0.0, 1.0, granularity).tolist()
-  probability_vectors = {(p,q): [p*q, p*(1-q), (1-p)*q, (1-p)*(1-q)] for p in probability_range for q in probability_range}
-  payoff_vector_1 = list(chain.from_iterable(payoff_matrix_p1))
-  payoff_vector_2 = list(chain.from_iterable(payoff_matrix_p2))
-  utilities_1 = {pair: sum(list(map(lambda x, y: x*y, probability_vectors[pair], payoff_vector_1))) for pair in probability_vectors}
-  utilities_2 = {pair: sum(list(map(lambda x, y: x*y, probability_vectors[pair], payoff_vector_2))) for pair in probability_vectors}
-  # print(utilities_1)
-  # print(utilities_2)
-  matched = list(map(lambda x, y: (x[0], abs(x[1] - y[1])), sorted(utilities_1.items()), sorted(utilities_2.items())))
-  minimum = min(matched, key=lambda x: x[1])
-  print([x for x in matched if x[1] == minimum[1]])
+  # Construct dict of probability vectors
+  row_dimension = len(payoff_matrix_p1)
+  col_dimension = len(payoff_matrix_p1[0])
+  hmm = [[p] for p in probability_range]
+  for m in range(row_dimension - 2):
+    hmm = [el + [p] for el in hmm for p in probability_range]
+  hmm = [el for el in hmm if sum(el) <= 1.0]
+  print(hmm)
+  vector = [operator(*el) for el in hmm]
+  print(vector)
+  # Other...
+  # probability_vectors = {(p,q): [p*q, p*(1-q), (1-p)*q, (1-p)*(1-q)] for p in probability_range for q in probability_range}
+  # payoff_vector_1 = list(chain.from_iterable(payoff_matrix_p1))
+  # payoff_vector_2 = list(chain.from_iterable(payoff_matrix_p2))
+  # utilities_1 = {pair: sum(list(map(lambda x, y: x*y, probability_vectors[pair], payoff_vector_1))) for pair in probability_vectors}
+  # utilities_2 = {pair: sum(list(map(lambda x, y: x*y, probability_vectors[pair], payoff_vector_2))) for pair in probability_vectors}
+  # # print(utilities_1)
+  # # print(utilities_2)
+  # matched = list(map(lambda x, y: (x[0], abs(x[1] - y[1])), sorted(utilities_1.items()), sorted(utilities_2.items())))
+  # minimum = min(matched, key=lambda x: x[1])
+  # print(sorted([x for x in matched if x[1] == minimum[1]], key=lambda x: x[0]))
   return None
 
 def test(condition):
@@ -58,8 +73,6 @@ if __name__ == '__main__':
   # Solve for PSNE
   psne = solve_psne_2(p_matrix_p1, p_matrix_p2)
   test(psne == [])
-  # Solve for MSNE
-  msne = solve_msne_2(p_matrix_p1, p_matrix_p2)
   ### Test scenario3: Example 4.16 from Carter's book
   # Create payoff matrices for two players
   p_matrix_p1 = [[1, 4, 2], [4, 0, 4], [2, 3, 5]]
@@ -67,4 +80,6 @@ if __name__ == '__main__':
   # Solve for PSNE
   psne = solve_psne_2(p_matrix_p1, p_matrix_p2)
   test(psne == [(2, 2)])
+  # Solve for MSNE
+  msne = solve_msne_2(p_matrix_p1, p_matrix_p2)
   
