4cfdc87089205e3a31f7c018233d78210f899631
Jakub Konka <kubkon@gmail.com>
2012-08-27 16:30:06 +0200

    Algorithm for finding MSNE in 2-player case with only two strategies per player works

--- simultaneous_move/msne.py
+++ simultaneous_move/msne.py
@@ -13,7 +13,19 @@ def get_probability_vectors(dimension, probability_range):
   return [convexify(*el) for el in vector]
 
 def multiply_probability_vectors(vector_1, vector_2):
-  return [v1*v2 for v1 in vector_1 for v2 in vector_2]
+  return tuple(v1*v2 for v1 in vector_1 for v2 in vector_2)
+
+def find_msne(intersections, length, player):
+  msne = None
+  for pair in intersections:
+    msne = pair[player]
+    msne_list = []
+    for pair in intersections:
+      if pair[player] == msne:
+        msne_list += [pair[(player + 1) % 2]]
+    if len(msne_list) == length:
+      break
+  return msne
 
 def solve_msne_2(payoff_matrix_p1, payoff_matrix_p2, granularity=5):
   # Construct list of probability values
@@ -22,20 +34,16 @@ def solve_msne_2(payoff_matrix_p1, payoff_matrix_p2, granularity=5):
   vector_p1 = get_probability_vectors(len(payoff_matrix_p1), probability_range)
   vector_p2 = get_probability_vectors(len(payoff_matrix_p1[0]), probability_range)
   # Construct utility vectors for each bidder
-  probability_vectors = [multiply_probability_vectors(v1, v2) for v1 in vector_p1 for v2 in vector_p2]
-  print(probability_vectors)
-  # Other...
-  # probability_vectors = {(p,q): [p*q, p*(1-q), (1-p)*q, (1-p)*(1-q)] for p in probability_range for q in probability_range}
-  # payoff_vector_1 = list(chain.from_iterable(payoff_matrix_p1))
-  # payoff_vector_2 = list(chain.from_iterable(payoff_matrix_p2))
-  # utilities_1 = {pair: sum(list(map(lambda x, y: x*y, probability_vectors[pair], payoff_vector_1))) for pair in probability_vectors}
-  # utilities_2 = {pair: sum(list(map(lambda x, y: x*y, probability_vectors[pair], payoff_vector_2))) for pair in probability_vectors}
-  # # print(utilities_1)
-  # # print(utilities_2)
-  # matched = list(map(lambda x, y: (x[0], abs(x[1] - y[1])), sorted(utilities_1.items()), sorted(utilities_2.items())))
-  # minimum = min(matched, key=lambda x: x[1])
-  # print(sorted([x for x in matched if x[1] == minimum[1]], key=lambda x: x[0]))
-  return None
+  probability_vectors = {(v1, v2): multiply_probability_vectors(v1, v2) for v1 in vector_p1 for v2 in vector_p2}
+  payoff_vector_p1 = list(chain.from_iterable(payoff_matrix_p1))
+  payoff_vector_p2 = list(chain.from_iterable(payoff_matrix_p2))
+  utilities_p1 = {pair: sum(list(map(lambda x,y: x*y, probability_vectors[pair], payoff_vector_p1))) for pair in probability_vectors}
+  utilities_p2 = {pair: sum(list(map(lambda x,y: x*y, probability_vectors[pair], payoff_vector_p2))) for pair in probability_vectors}
+  intersections = {pair: abs(utilities_p1[pair] - utilities_p2[pair]) for pair in probability_vectors}
+  minimum = min(intersections.values())
+  intersections = {pair: intersections[pair] for pair in intersections if intersections[pair] == minimum}
+  msne = (find_msne(intersections, len(probability_range), 0), find_msne(intersections, len(probability_range), 1))
+  return msne
 
 def test(condition):
   try:
@@ -61,9 +69,10 @@ if __name__ == '__main__':
   p_matrix_p2 = [[1, -1], [-1, 1]]
   # Solve for MSNE
   msne = solve_msne_2(p_matrix_p1, p_matrix_p2)
+  test(((.5, .5), (.5, .5)) == msne)
   ### Test scenario2: Example 4.16 from Carter's book
   # Create payoff matrices for two players
   p_matrix_p1 = [[1, 4, 2], [4, 0, 4]]#, [2, 3, 5]]
   p_matrix_p2 = [[3, 2, 2], [0, 3, 1]]#, [5, 4, 6]]
   # Solve for MSNE
-  msne = solve_msne_2(p_matrix_p1, p_matrix_p2)
+  # msne = solve_msne_2(p_matrix_p1, p_matrix_p2)
