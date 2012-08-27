566c19dc4feb93be64f8bd19ab8e30013834e0ab
Jakub Konka <kubkon@gmail.com>
2012-08-27 23:10:19 +0200

    Drafting GUI

---
+++ run.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from tkinter import ttk
+
+def create_form(*args):
+  pass
+
+if __name__ == '__main__':
+  root = Tk()
+  root.title("PyGameTheory")
+  
+  mainframe = ttk.Frame(root, padding="3 3 12 12")
+  mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+  mainframe.columnconfigure(0, weight=1)
+  mainframe.rowconfigure(0, weight=1)
+  
+  row = StringVar()
+  column = StringVar()
+  
+  row_entry = ttk.Entry(mainframe, width=4, textvariable=row)
+  row_entry.grid(column=1, row=1, sticky=(W, E))
+  column_entry = ttk.Entry(mainframe, width=4, textvariable=column)
+  column_entry.grid(column=1, row=2, sticky=(W, E))
+  
+  ttk.Button(mainframe, text="Create form", command=create_form).grid(column=2, row=3, sticky=W)
+  
+  ttk.Label(mainframe, text="rows").grid(column=2, row=1, sticky=W)
+  ttk.Label(mainframe, text="columns").grid(column=2, row=2, sticky=W)
+  
+  root.mainloop()
+  
